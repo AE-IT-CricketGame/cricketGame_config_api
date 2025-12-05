87bb448ab64e972d4b35485422668379089b1b0f
nexzanavod <aanavod@gmail.com>
2025-12-05 11:50:19 +0530

    Stop cron job in reminder sms

--- src/app.service.ts
+++ src/app.service.ts
@@ -630,7 +630,7 @@ export class AppService {
     }
   }
 
-  @Cron(CronExpression.EVERY_DAY_AT_NOON)
+  // @Cron(CronExpression.EVERY_DAY_AT_NOON)
   async sendReminder() {
     const chunkSize = 10;
     const dialogChunks = [];
